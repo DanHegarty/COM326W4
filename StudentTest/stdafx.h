77678bacce7cc05a544b0b4b6735eb94741b4ca8
Daniel Hegarty <hegarty-d8@ulster.ac.uk>
2018-11-15 10:44:24 +0000

    Added Unit Test

---
+++ StudentTest/stdafx.h
@@ -0,0 +1,21 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+
+// TODO: reference additional headers your program requires here
+#include "../Classes Challenges/Student.h"
+#include "../Classes Challenges/Module.h"
+#include "../RPG/Archer.h"
+#include "../RPG/Armour.h"
+#include "../RPG/Item.h"
+#include "../RPG/Warrior.h" 
+#include "../RPG/Weapon.h" 
+#include "../RPG/Wizard.h"
---
+++ StudentTest/unittest1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace StudentTest
+{		
+	TEST_CLASS(TestModuleClass)
+	{
+	public:
+		
+		TEST_METHOD(TestDefaultConstructor)
+		{
+			// TODO: Your test code here
+
+
+		}
+
+	};
+}
